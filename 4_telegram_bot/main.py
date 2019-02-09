93375d426c01b192075624f41467ad1955e73157
Pablo Satler <pablosatler@gmail.com>
2019-02-09 14:21:04 -0200

    chore: :ok_hand: updating code to use class and methods

--- 4_telegram_bot/main.py
+++ 4_telegram_bot/main.py
@@ -7,31 +7,35 @@ from dotenv import load_dotenv
 load_dotenv() # to load .env to environment variables, so we can use os.environ
 
 
-# This function fetches the dog public API to get a dog url (image, gif, etc)
-def get_url():
-    # accessing the API and getting json data
-    URL = 'https://random.dog/woof.json'
-    contents = requests.get(URL).json()
-    image_url = contents['url'] # getting the url key from the json above
-    return image_url
+class Bot_Handler:
+    def __init__(self, token):
+        self.token = token
+        self.telegram_api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.dog_api_url = 'https://random.dog/woof.json'
 
-def get_media_or_photo_url():
-    allowed_extension = ['jpg','jpeg','png', 'gif']
-    file_extension = ''
-    while file_extension not in allowed_extension:
-        url = get_url()
-        file_extension = re.search("([^.]*)$",url).group(1).lower()
-    return url, file_extension
+    # This method fetches the dog public API to get a dog url (image, gif, etc)
+    def get_url(self):
+        contents = requests.get(self.dog_api_url).json() # accessing the API and getting json data
+        image_url = contents['url'] # getting the url key from the json above
+        return image_url
 
-def get_doggo(bot, update):
-    # url = get_image_url()
-    url, file_extension = get_media_or_photo_url()
-    chat_id = update.message.chat_id
-    
-    if file_extension == 'gif': # https://python-telegram-bot.readthedocs.io/en/stable/telegram.bot.html?highlight=send_photo#telegram.Bot.send_animation
-        bot.send_animation(chat_id=chat_id, animation=url)
-    else: # https://python-telegram-bot.readthedocs.io/en/stable/telegram.bot.html?highlight=send_photo#telegram.Bot.send_photo
-        bot.send_photo(chat_id=chat_id, photo=url)
+    def get_media_or_photo_url(self):
+        allowed_extension = ['jpg','jpeg','png', 'gif']
+        file_extension = ''
+        while file_extension not in allowed_extension:
+            url = self.get_url() # using above method
+            file_extension = re.search("([^.]*)$",url).group(1).lower()
+        return url, file_extension
+
+    def get_doggo(self, bot, update):
+        url, file_extension = self.get_media_or_photo_url()
+        chat_id = update.message.chat_id
+        print('chat_id', chat_id)
+        
+        if file_extension == 'gif': # https://python-telegram-bot.readthedocs.io/en/stable/telegram.bot.html?highlight=send_photo#telegram.Bot.send_animation
+            bot.send_animation(chat_id=chat_id, animation=url)
+        else: # https://python-telegram-bot.readthedocs.io/en/stable/telegram.bot.html?highlight=send_photo#telegram.Bot.send_photo
+            bot.send_photo(chat_id=chat_id, photo=url)
 
 
 #######################
@@ -40,9 +44,10 @@ def get_doggo(bot, update):
 
 def main():
     TOKEN = os.environ['DOG_API_TOKEN']
+    bot = Bot_Handler(TOKEN)
     updater = Updater(TOKEN)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('doggo', get_doggo))
+    dp.add_handler(CommandHandler('doggo', bot.get_doggo))
     updater.start_polling()
     updater.idle()
 
